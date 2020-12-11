c8a4fc20530868178c3eabdc268dfb1ecb952bb3
pearlslugs <pearlslugs@gmail.com>
2020-12-11 11:48:19 -0600

    reviews page start

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
-import React from "react";
+import renderToString from "next-mdx-remote/render-to-string";
+import hydrate from "next-mdx-remote/hydrate";
+
+import services from "../src/communication/services";
+
+import matter from "gray-matter";
+
 import {
   Box,
   Image,
@@ -16,30 +22,53 @@ import {
   IconButton,
 } from "@chakra-ui/react";
 import { CopyIcon } from "@chakra-ui/icons";
-import services from "../src/communication/services";
 
-
-const HomePage = ({ template }) => {
-  const testDb = (id) => {
-    console.log("test");
-    services.getTemplates(id).then((res) => console.log(res, "res"));
-  };
-  const pageContent = content;
-  const templateNum = pageContent.Template_Number;
-  console.log(templateNum);
-  if (templateNum == 1) {
-    console.log("good");
-
-    return (
-      <div>
-
-        <button onClick={() => testDb(1)}>Test</button>
-      </div>
-    );
-  } else {
-    console.log("something");
-    return null;
-  }
+const components = {
+  Box,
+  Image,
+  Link,
+  List,
+  Heading,
+  Text,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Button,
+  Divider,
+  ListItem,
+  IconButton,
+  CopyIcon,
 };
 
-export default HomePage;
+export default function HomePage({ source }) {
+  const content = hydrate(source, { components });
+
+  return <div className="wrapper">{content}</div>;
+}
+
+export async function getStaticProps() {
+  const fs = require("fs");
+  const yaml = require('js-yaml')
+
+  let personal = await yaml.safeLoad(fs.readFileSync('src/content/Personal-Settings.yml', 'utf8'));
+  let yamlContent = await fs.readFileSync("src/content/content.yml", "utf8");
+  let altText = await fs.readFileSync("src/content/alt-tags.yml", "utf8");
+  let colors = await fs.readFileSync("src/content/colors.yml", "utf8");
+
+  const template = await services.getTemplates(personal.template)
+  .then((data) => {
+    return data;
+  });
+  const filteredYamlContent = yamlContent.split("---").join(' ')
+  const filteredAltText = altText.split("---").join(' ')
+  const filteredColors = colors.split("---").join(' ')
+  const source = `---\n${filteredYamlContent}\n${filteredAltText}\n${filteredColors}\n--- ${template}`;
+  console.log(source)
+  const { content, data } = matter(source);
+  const mdxSource = await renderToString(content, {
+    components,
+    scope: data,
+  });
+  return { props: { source: mdxSource, frontMatter: data } };
+}
